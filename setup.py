cda3205d45c567d3a73c05bc0db99426e12e499d
stellasia <stellaou13@gmail.com>
2015-07-17 13:22:27 +0200

    Use python package structure and prepare for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-pandasjsonfield',
+  packages = ['djangopandasjsonfield'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Django model field for pandas objects (Series and DataFrame)',
+  author = 'Estelle Scifo',
+  author_email = '',
+  url = '',
+  download_url = '',
+  keywords = ['django', 'field', 'pandas'],
+  classifiers = [],
+)
