39aa65668035f23d6e45abf9ec0b07651e057500
stellasia <stellasia@devience.fr>
2015-07-19 21:12:37 +0200

    Complete setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'django-pandasjsonfield',
-  packages = ['djangopandasjsonfield'], # this must be the same as the name above
+  name = 'pandasjsonfield',
+  packages = ['pandasjsonfield'],
   version = '0.1',
   description = 'Django model field for pandas objects (Series and DataFrame)',
   author = 'Estelle Scifo',
   author_email = '',
-  url = '',
-  download_url = '',
+  url = 'https://github.com/stellasia/django-pandasjsonfield',
+  download_url = 'https://github.com/stellasia/django-pandasjsonfield/archive/0.1.tar.gz',
   keywords = ['django', 'field', 'pandas'],
   classifiers = [],
 )
