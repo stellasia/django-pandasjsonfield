864116d53be140c3299db0968f6447ba6b5536d4
stellasia <stellasia@devience.fr>
2015-07-22 23:31:36 +0200

    PEP8 compliance + cleaning

--- pandasjsonfield/fields.py
+++ pandasjsonfield/fields.py
@@ -5,19 +5,17 @@ import pandas as pd
 
 
 class PandasJSONField(models.TextField):
-    """ Pandas Series or distribution 
-    saved in JSON format in a TextField. 
-    
-    Additionnal parameter : `typ` = 'serie'|'frame' 
-    (if not specified, use the pandas default, 
-    currently 'frame' in version 0.16.2)
+    """ Pandas Series or distribution
+    saved in JSON format in a TextField.
+
+    Additionnal parameter : `typ` = 'serie'|'frame'
     """
 
     def __init__(self, *args, **kwargs):
         try:
             self._typ = kwargs.pop("typ")
         except KeyError:
-            self._typ = 'frame'
+            self._typ = None
         super(PandasJSONField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
@@ -27,17 +25,15 @@ class PandasJSONField(models.TextField):
 
     # Convert text field (value) to python object
     def from_db_value(self, value, expression, connection, context):
-        print "from_db_value", value
         if value is None:
             return value
         return pd.read_json(value, typ=self._typ)
 
     def to_python(self, value):
-        print "to_python", value
         if value is None:
             return value
-        if isinstance(value, pd.Series):
-            return value        
+        if isinstance(value, pd.Series) or isinstance(value, pd.DataFrame):
+            return value
         try:
             return pd.read_json(value, typ=self._typ)
         except:
@@ -45,14 +41,12 @@ class PandasJSONField(models.TextField):
 
     # convert python object to db type
     def get_prep_value(self, value):
-        print "get_prep_value"
-        if value :
+        if value:
             return value.to_json()
         return value
 
     def get_db_prep_value(self, value, connection, prepared=False):
         """Convert JSON object to a string"""
-        print "get_db_prep_value", value
         if self.null and value is None:
             return None
         return value.to_json()
--- pandasjsonfield/models.py
+++ pandasjsonfield/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
 from .fields import PandasJSONField
 
 
 class MyModel(models.Model):
     serie = PandasJSONField(typ="serie", null=True)
-    dataframe = PandasJSONField(null=True)
-    
+    dataframe = PandasJSONField(typ="frame", null=True)
+
     def mean(self, column):
         try:
             return self.dataframe[column].mean()
         except KeyError:
             return None
-
